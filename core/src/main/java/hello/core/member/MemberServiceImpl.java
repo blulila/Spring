4d1b1c8dd567620ffee1f2b4d392c49294a13e15
blulila <mgparkcs@gmail.com>
2021-07-30 00:12:20 +0900

    AppConfig - DIP / OCP

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //command+shift+enter, DIP 위반
+//    private final MemberRepository memberRepository = new MemoryMemberRepository(); //command+shift+enter, DIP 위반
+    private final MemberRepository memberRepository;
+
+    //command+n-generate
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
