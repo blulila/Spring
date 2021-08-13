b68aba7708b8f36dbb0d228d9558bf6e523260a9
blulila <mgparkcs@gmail.com>
2021-08-13 19:15:47 +0900

    oauth

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,21 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다")
+    void findBeanByTypeDuplicate(){
+        ac.getBean(DiscountPolicy.class)
+    }
+
+    @Configuration
+    static class SameB
+}
--- security/src/main/java/springboot/security/repository/UserRepository.java
+++ security/src/main/java/springboot/security/repository/UserRepository.java
@@ -4,13 +4,16 @@ import springboot.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-//CRUD 함수를 JpaRepository가 들고 있으며, @Repository라는 어노테이션이 없어도 IOC 가능
-// JpaRepository 를 상속하면 자동 컴포넌트 스캔됨.
+//CRUD 함수를 JpaRepository가 들고 있으며, @Repository라는 어노테이션이 없어도 IOC 가능 -> JpaRepository를 상속했기 때문
+//JpaRepository 를 상속하면 자동 컴포넌트 스캔됨.
 public interface UserRepository extends JpaRepository<User, Integer>{
+    //findBy규칙 -> Username 문법
+    //select* from user where username = 1?
 
     // Jpa Naming 전략
     // SELECT * FROM user WHERE username = 1?
-    User findByUsername(String username);
+    public User findByUsername(String username); // Jpa Query methods
+
     // SELECT * FROM user WHERE username = 1? AND password = 2?
     // User findByUsernameAndPassword(String username, String password);
 
